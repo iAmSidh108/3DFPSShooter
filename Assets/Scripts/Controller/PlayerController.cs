e5011d78a4452ded6cce85508ea667f46b96b4e8
Siddharth Tiwari <sidh589@gmail.com>
2021-12-18 23:39:52 +0530

    Added MoveWhileJump and FullViewRotation
    In the last commit player could jump but it could not move while jumping, so added code to make player do so.
    
    Also in the last commit player could only rotate up and down, so added code to make it happen all around i.e. 360.

--- Assets/Scripts/Controller/PlayerController.cs
+++ Assets/Scripts/Controller/PlayerController.cs
@@ -64,6 +64,7 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         ProcessMovement();
+        ProcessRotation();
     }
 
     Vector3 moveDirection;
@@ -90,10 +91,24 @@ public class PlayerController : MonoBehaviour
                 moveDirection.y = jumpPower;
             }
         }
+        else
+        {
+            moveDirection = new Vector3(leftRightInput * moveSpeed, moveDirection.y, forwardBackwardInput * moveSpeed);
+            moveDirection = transform.TransformDirection(moveDirection);
+        }
+
+
         moveDirection.y -= gravity*Time.deltaTime;
         controller.Move(moveDirection * Time.deltaTime);
 
     }
 
+    void ProcessRotation()
+    {
+        float horizontalLookInput = inputManager.horizontalLookAxis;
+        Vector3 playerRotation = transform.rotation.eulerAngles;
+        transform.rotation = Quaternion.Euler(new Vector3(playerRotation.x, playerRotation.y + horizontalLookInput * lookSpeed * Time.deltaTime
+            , playerRotation.z));
+    }
 
 }
