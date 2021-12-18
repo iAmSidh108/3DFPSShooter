61d7f37cf210e205089745b85f7be71fdc09ec67
Siddharth Tiwari <sidh589@gmail.com>
2021-12-19 01:56:07 +0530

    Revert "Created final player controller."
    This reverts commit b31df03d6244597eaf0e1692c4d02f6375c34437.

--- Assets/Scripts/Controller/PlayerController.cs
+++ Assets/Scripts/Controller/PlayerController.cs
@@ -24,8 +24,6 @@ public class PlayerController : MonoBehaviour
     [Header("Required References")]
     [Tooltip("The player shooter script that fires projectiles.")]
     public Shooter playerShooter;
-    public Health playerHealth;
-    public List<GameObject> disableWhileDead;
 
     private CharacterController controller;
     private InputManager inputManager;
@@ -70,23 +68,6 @@ public class PlayerController : MonoBehaviour
     /// </summary>
     void Update()
     {
-        if (playerHealth.currentHealth <= 0)
-        {
-            foreach (GameObject inGameObject in disableWhileDead)
-            {
-                inGameObject.SetActive(false);
-            }
-            return;
-        }
-        else
-        {
-            foreach (GameObject inGameObject in disableWhileDead)
-            {
-                inGameObject.SetActive(true);
-            }
-        }
-
-
         ProcessMovement();
         ProcessRotation();
     }
